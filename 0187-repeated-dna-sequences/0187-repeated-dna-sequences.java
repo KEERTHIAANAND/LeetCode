8d936a298903866ead7a89fc36cfc4a608a872a0
KEERTHI AANAND K S <keerthiaanand.ks2024cse@sece.ac.in>
2026-06-19 10:53:12 +0530

    Time: 17 ms (94.11%), Space: 55.7 MB (55.86%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        HashSet<String> seen = new HashSet<>();
+        HashSet<String> reported = new HashSet<>();
+        for(int i = 0; i<=s.length() - 10; i++){
+            String substring = s.substring(i, i+10);
+            if(!seen.add(substring)){
+                reported.add(substring);
+            }
+        }
+        return new ArrayList<>(reported);
+    }
+}
